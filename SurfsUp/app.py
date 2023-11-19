3a708edf9b8e3155daee1351d5be459b046d9cbb
Pasin <89669387+Sindu7@users.noreply.github.com>
2023-11-19 14:56:56 -0800

    done

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,6 @@
 # Import the dependencies.
 import numpy as np
+import datetime as dt
 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -12,7 +13,6 @@ from flask import Flask, jsonify
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -21,7 +21,6 @@ Base.prepare(autoload_with=engine)
 # Save references to each table
 station = Base.classes.station
 measurement = Base.classes.measurement
-
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
@@ -37,3 +36,117 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+         f"/api/v1.0/stations<br/>"
+          f"/api/v1.0/tobs<br/>"
+           f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(station.station).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    results = session.query(measurement.prcp, measurement.date).\
+    filter(measurement.date >= '2016-08-23').all()
+
+    session.close()
+
+    # Create a dictionary from the row data
+    all_results = []
+    for prcp, date in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        all_results.append(prcp_dict)
+
+    return jsonify(all_results)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(measurement.date, measurement.tobs).\
+    filter(measurement.station == 'USC00519281').\
+    filter(measurement.date >= prev_year).all()
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_results = []
+    for date, tobs in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["tobs"] = tobs
+        all_results.append(prcp_dict)
+
+    return jsonify(all_results)
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+     filter(measurement.date >= start).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+     filter(measurement.date >= start).filter(measurement.date <= end).all()
+
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
